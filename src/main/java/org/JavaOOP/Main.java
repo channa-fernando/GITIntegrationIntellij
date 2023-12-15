ca1a06c67fc821c728c229eb6dee61cfda39e592
Channa <fchanna2853@gmail.com>
2023-12-15 18:55:02 +0530

    Commit Main Java file from local environment to remote repository

--- src/main/java/org/JavaOOP/Main.java
+++ src/main/java/org/JavaOOP/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         String firstName = "Michael";
-        String lastName = " Jackson";
+        String lastName = " White";
 
-        System.out.println("Hello world!");
+        System.out.println("Combined Name: " + firstName + lastName);
     }
 }
 
